03332922cda4609446b6910db623f375cb8133c8
Yul <vaimer198@gmail.com>
2024-03-09 20:53:39 +0700

    Initial Commit

---
+++ GP6_Ivanova_Todolist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GP6_Ivanova_Todolist
+//
+//  Created by Юлия Иванова on 09.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GP6_Ivanova_Todolist/GP6_Ivanova_TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  GP6_Ivanova_TodolistApp.swift
+//  GP6_Ivanova_Todolist
+//
+//  Created by Юлия Иванова on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GP6_Ivanova_TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
